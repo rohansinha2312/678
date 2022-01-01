72e9985fa70fdb527d227c01ae7172f772333684
Shobha <raj@rajs-air.myfiosgateway.com>
2022-01-01 12:28:58 -0500

    message

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import gpt3Logo from '../../logo.svg';
 import './footer.css';
 
 const Footer = () => (
@@ -9,12 +8,12 @@ const Footer = () => (
     </div>
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
-        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p className="header">XSTEM</p>
       </div>
       <div className="gpt3__footer-links_div">
       <p><a href="#home">Home</a></p>
           <p><a href="#wgpt3">About</a></p>
-          <p><a href="#possibility">Pedagogy</a></p>
+          <p><a href="#possibility">Club</a></p>
           <p><a href="#features">Work</a></p>
           <p><a href="#blog">Challenges</a></p>
           <p>
@@ -27,22 +26,11 @@ const Footer = () => (
             </a>
           </p>
       </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Company</h4>
-        <p>Terms & Conditions </p>
-        <p>Privacy Policy</p>
-        <p>Contact</p>
-      </div>
-      <div className="gpt3__footer-links_div">
-        <h4>Get in touch</h4>
-        <p>info@payme.net</p>
-      </div>
     </div>
 
     <div className="gpt3__footer-copyright">
       <p>
-        <a href="https://mahitm.com" target="_blank" rel="noreferrer noopener">@</a>
-        2022 XSTEM All rights reserved.</p>
+        @2022 XSTEM All rights reserved.</p>
     </div>
   </div>
 );
