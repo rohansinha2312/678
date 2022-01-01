71d9c28b07939d56ae681db9ce3d704648228cd9
Shobha <raj@Rajs-MacBook-Air.local>
2022-01-01 10:25:57 -0500

    message

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Article from '../../components/article/Article';
-import { blog01, blog02, blog03, blog04, blog05} from './imports';
+import { blog01, blog02, blog03, blog04, blog05, blog06, blog07, blog08, blog09} from './imports';
 import './blog.css';
 
 const Blog = () => (
@@ -17,10 +17,10 @@ const Blog = () => (
         <Article href="https://blueoceancompetition.org/" imgUrl={blog03} date="Feb 8, 2022" text="Blue Ocean" />
         <Article href="https://www.nfte.com/fall-2021-nfte-world-series-of-innovation/" imgUrl={blog04} date="Dec 15, 2022" text="NFTE World Series of Innovation" />
         <Article href="https://www.projectparadigm.org/#" imgUrl={blog05} date="May 1, 2022" text="Paradigm Challenge (age restriction)" />
-        <Article href="https://technovationchallenge.org/get-started/" imgUrl={blog05} date="Mar 25, 2022" text="Technovation Challange (Girls Only)" />
-        <Article href="https://www.cooperhewitt.org/national-design-awards/national-design-awards-submission-guidelines/#:~:text=Submission%20Deadline,11%3A59%20p.m.%20Eastern%20Time." imgUrl={blog02} date="Feb 22, 2022" text="Cooper Hewitt" />
-        <Article href="https://www.google.com/url?q=https://diamondchallenge.org/summit/&sa=D&source=editors&ust=1641028739470951&usg=AOvVaw3ll6klLUQRTiC0rbYIyW0R" imgUrl={blog03} date="Jan 17, 2022" text="Diamond Challenge" />
-        <Article href="https://www.businessbootcamp101.org/pitchnj-1" imgUrl={blog04} date="Jan 1, 2022" text="Pitch NJ" />
+        <Article href="https://technovationchallenge.org/get-started/" imgUrl={blog06} date="Mar 25, 2022" text="Technovation Challange (Girls Only)" />
+        <Article href="https://www.cooperhewitt.org/national-design-awards/national-design-awards-submission-guidelines/#:~:text=Submission%20Deadline,11%3A59%20p.m.%20Eastern%20Time." imgUrl={blog07} date="Feb 22, 2022" text="Cooper Hewitt" />
+        <Article href="https://www.google.com/url?q=https://diamondchallenge.org/summit/&sa=D&source=editors&ust=1641028739470951&usg=AOvVaw3ll6klLUQRTiC0rbYIyW0R" imgUrl={blog08} date="Jan 17, 2022" text="Diamond Challenge" />
+        <Article href="https://www.businessbootcamp101.org/pitchnj-1" imgUrl={blog09} date="Jan 1, 2022" text="Pitch NJ" />
       </div>
     </div>
   </div>
--- src/containers/blog/imports.js
+++ src/containers/blog/imports.js
@@ -3,11 +3,18 @@ import blog02 from '../../assets/blog02.png';
 import blog03 from '../../assets/blog03.png';
 import blog04 from '../../assets/blog04.png';
 import blog05 from '../../assets/blog05.png';
-
+import blog06 from '../../assets/blog06.png';
+import blog07 from '../../assets/blog07.png';
+import blog08 from '../../assets/blog08.png';
+import blog09 from '../../assets/blog09.png';
 export {
   blog01,
   blog02,
   blog03,
   blog04,
   blog05,
+  blog06,
+  blog07,
+  blog08,
+  blog09,
 };
