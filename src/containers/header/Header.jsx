2bc414af04a396a5ff1917d2b98b0d0e620f5f18
Shobha <raj@rajs-air.myfiosgateway.com>
2022-01-03 18:10:11 -0500

    heelo

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,7 +6,7 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content" data-tilt>
-      <h1 className="gradient__text" data-tilt>Welcome to<br/> XSTEM</h1>
+      <h1 className="gradient__text" data-tilt>Welcome to XSTEM<br/>Join us and Explore Your Possibilites</h1>
       <p>An entrepreneurship club incorporating STEM knowledge</p>
       <div className="gpt3__header-content__people" data-tilt>
       </div>
