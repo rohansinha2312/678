ee153fc131683d227ef14e14b7e3349e2fa6d497
RISHABH <RISHABH@news-mbp-5.myfiosgateway.com>
2022-01-12 15:58:27 -0500

    awdawda

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,7 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content" data-tilt>
-      <h1 className="gradient__text" data-tilt>Welcome to XSTEM bitch<br/>Join us and Explore Your Possibilites</h1>
+      <h1 className="gradient__text" data-tilt>Welcome to XSTEM biwadawdtch<br/>Join us and Explore Your Possibilites</h1>
       <p>An entrepreneurship club incorporating STEM knowledge</p>
       <div className="gpt3__header-content__people" data-tilt>
       </div>
